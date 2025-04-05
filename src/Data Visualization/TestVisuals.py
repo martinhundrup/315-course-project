bab9c331870fd28528b7e2896df5dc296a0fa05a
Nick2k4L <nicholas.b.lopez@wsu.edu>
2025-04-05 15:49:31 -0700

    able to get the data visualization testing working

--- src/Data Visualization/TestVisuals.py
+++ src/Data Visualization/TestVisuals.py
@@ -11,21 +11,18 @@ import os
 
 def test_visuals():
     # here we well be trying to visualize the Covid cases vs deaths in the US Counties using Plotly Express
-    dataFrame = pd.read_csv('C:\\Users\\13606\\Desktop\\CPT_S 315 Data Project\\315-course-project\\us-counties-2023.csv')
+    dataFrame = pd.read_csv('C:\\Users\\13606\\Desktop\\CPT_S 315 Data Project\\315-course-project\\us-counties-2023.csv',
+     dtype={"fips": str})
 
-    dataFrame['fips'] = dataFrame['fips'].astype(str)
+    #dataFrame['fips'] = dataFrame['fips'].astype(str)
     # above is reading the csv from the github
-    dataFrame['deaths'] = dataFrame['deaths']
+    #dataFrame['deaths'] = dataFrame['deaths']
 
-    figure = px.choropleth_map(dataFrame, geojson=counties, locations='fips', color='deaths',
-                           color_continuous_scale="Viridis",
-                           range_color=(0, 10),
-                           map_style="carto-positron",
-                           zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
-                           opacity=0.5,
-                           labels={'unemp':'unemployment rate'}
-                          )
-    figure.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    figure = px.choropleth(dataFrame, geojson=counties, locations='fips', color='deaths',color_continuous_scale="inferno",
+                           range_color=(0,20000), scope="usa", hover_name='county', labels='deaths')
+
+    
+    #figure.update_geos(fitbounds='locations', visible=True)
     figure.show()
 
 

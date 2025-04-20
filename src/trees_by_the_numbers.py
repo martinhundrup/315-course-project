8e8d9e01e4b7abfcdc66b1b99f3301a58d6347fb
JamesRtoP <yoilbob@outlook.com>
2025-04-19 23:33:46 -0700

    Better results by the numbers

--- src/trees_by_the_numbers.py
+++ src/trees_by_the_numbers.py
@@ -6,26 +6,17 @@ import numpy as np
 allData = pd.read_csv("./src/ppdata.csv")
 allData = allData.drop(columns = ["State","County","FIPS"])
 
-allData.corr().to_csv("Coorelation-Population.csv")
 
-
-allData = allData.drop(columns = ["Poverty Raw","Unemployed",
-                        "Less than high school graduate, 2019-23",
-        "High school graduate (or equivalency), 2019-23",
-        "Some college or associate degree, 2019-23",
-        "Bachelor's degree or higher, 2019-23"
-                        ])
 allData = allData.dropna()
 percentCasesDeaths = allData["COVID Deaths"].div(allData["COVID Cases"])#.rename("Percent Deaths of Cases"))
-allData[[
-    "COVID Cases","COVID Deaths","Labor Force","Employed",
-         ]] = allData[[
-    "COVID Cases","COVID Deaths","Labor Force","Employed",
-         ]].div(allData["Population"], axis = 0)
-allData = allData.drop(columns = ["Population"])
+#allData["COVID Cases"] = allData["COVID Cases"].round()
+#allData[
+#    "COVID Cases"
+#         ] = allData[
+#   "COVID Cases"
+#        ].div(allData["Population"], axis = 0)
+#allData = allData.drop(columns = ["Population"])
 #print(allData.corr())
-allData.insert(1, "Percent Deaths of Cases",percentCasesDeaths)
-allData.corr().to_csv("Coorelation-Percent.csv")
 #allData = allData.drop(columns = ["Poverty Raw","Labor Force","Less than high school graduate, 2019-23","High school graduate (or equivalency), 2019-23","Some college or associate degree, 2019-23","Bachelor's degree or higher, 2019-23","Percent of adults who are not high school graduates, 2019-23","Percent of adults who are high school graduates (or equivalent), 2019-23","Percent of adults completing some college or associate degree, 2019-23","Percent of adults with a bachelor's degree or higher, 2019-23"])
 
 
@@ -39,8 +30,8 @@ from sklearn.metrics import accuracy_score,mean_squared_error,r2_score
 #equal frequency binning 
 median = allData["COVID Cases"].median()
 labels = ['low','medium','high','really fucking high']
-bins = [0,allData["COVID Cases"].quantile(0.30),allData["COVID Cases"].quantile(0.60),allData["COVID Cases"].quantile(0.9),1]
-
+bins = [0,allData["COVID Cases"].quantile(0.30),allData["COVID Cases"].quantile(0.60),allData["COVID Cases"].quantile(0.9),allData["COVID Cases"].max()]
+print(bins)
 #labels = ['low','medium','high']
 #bins = [0,allData["COVID Cases"].quantile(0.33),allData["COVID Cases"].quantile(0.66),1]
 
@@ -49,7 +40,7 @@ allData["COVID Cases"] = pd.cut(allData["COVID Cases"], bins = bins, labels = la
 
 allData = allData.dropna()
 
-X = allData.drop(columns = ["COVID Cases","COVID Deaths","Percent Deaths of Cases"])
+X = allData.drop(columns = ["COVID Cases","COVID Deaths"])
 map_label_to_int = {name: n for n, name in enumerate(labels)}
 Y = allData["COVID Cases"].replace(map_label_to_int)
 
@@ -128,7 +119,7 @@ best_params = grid_search.best_params_
 best_tree = grid_search.best_estimator_
 
 y_predicted = best_tree.predict(X_test)
-print("Best Random Forest Regressor")
+print("Best Random Forest Classifier")
 print("Accuracy= ", accuracy_score(y_test,y_predicted))
 print("R2 Score = ", r2_score(y_test,y_predicted))
 print("Mean Squared = ",mean_squared_error(y_test,y_predicted))
